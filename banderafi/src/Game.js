dffb5abd840029eeb178d3c97f6b55fd25f49627
mattsirota <mattsirota@msn.com>
2021-04-06 17:53:31 -0400

    created countries dictionary and functionality for game

--- banderafi/src/Game.js
+++ banderafi/src/Game.js
@@ -1,9 +1,85 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import CountryList from './Countries';
 
+let answered = false;
 function Game() {
+    const [flags, setFlags] = useState([]);
+    const [correctAnswer, setAnswer] = useState("");
+    const [score, setCount] = useState(0);
+
+    useEffect(() => {
+        getFlags();
+    }, [])
+    function getFlags()
+    {
+        answered = false;
+        let flagsList = [];
+        let answerIndex = Math.floor(Math.random() * 4)
+        for (let i = 0; i < 4; i++)
+        {
+            let flagIndex = Math.floor(Math.random() * CountryList.length);
+            let curCountry = CountryList[flagIndex];
+            let image = "https://www.countryflags.io/"+curCountry.code+"/flat/64.png";
+            let name = curCountry.name;
+            let answer = false;
+            if (i === answerIndex)
+            {
+                answer = true;
+                setAnswer(curCountry.name);
+            }
+            flagsList[i] = {
+                name: name,
+                image: image,
+                answer: answer
+            };
+        }
+        setFlags(flagsList);
+    }
+    function checkAnswer(name) 
+    {
+        if (answered === false)
+        {
+            answered = true;
+            console.log(name);
+            document.getElementById(correctAnswer).style.background = '#00FF00';
+
+            if(name !== correctAnswer)
+            {
+                let button = document.getElementById(name);
+                button.style.background = '#FF0000'
+            }
+            else
+                setCount(score+1);
+        }
+    }
+    function newFlags()
+    {
+        if (answered === true)
+            getFlags();
+    }
     return (
-        <p>Game Page</p>
+        <div className='game_area'>
+            <div className='question_area'>
+                <p>Which of the following is {correctAnswer}'s flag?</p>
+            </div>
+            <div className='flags_display'>
+                <div className='flags'>
+                    {
+                        flags.map((flag) => 
+                            <button key={flag.name} id={flag.name}>
+                                <img src={flag.image} alt="flag" onClick={() => checkAnswer(flag.name)}/>
+                            </button>)
+                    }
+                </div>
+            </div>
+            <div className='next_button'>
+            <button type="button" id="next" onClick={newFlags}>Continue</button>
+            </div>
+            <div className='score_area'>
+                <p>Score: {score}</p>
+            </div>
+        </div>
     )
 }
 
