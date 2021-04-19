00050a60395c2b53ad1eb482fa4b749226a7ae62
kyle <kyle.bernardes@gmail.com>
2021-04-19 18:58:50 -0400

    login branch created

--- banderafi/src/Login.js
+++ banderafi/src/Login.js
@@ -3,7 +3,22 @@ import './App.css';
 
 function Login() {
     return (
-        <p>Login Page</p>
+        <div className="login-page">
+            <h1>Login</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text"/>
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password"/>
+                </label>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
