f66d3cab2d8c5268cc64a39a5fcc30c37d27e0df
coriwisniewski <43626560+coriwisniewski@users.noreply.github.com>
2021-04-28 10:20:04 -0400

    responsive nav bar + other random styles

--- banderafi/src/Nav.js
+++ banderafi/src/Nav.js
@@ -1,31 +1,53 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
-import Login from './pages/Login';
 import {AuthContext} from './Auth';
 import SignOutButton from './SignOut';
+import menuicon from './menuicon.png';
 
 function Nav(props){
     const [user, setUser] = useContext(AuthContext);
+    const [open, setOpen] = useState(false);
+    let {firstName, ...rest} = user;
     let userText;
     let gameText;
+    let modifiedNav;
     if (!user){
         //no logged in user
-        userText = <li id="usertext">Welcome! <a href='./login'>Log In</a> or <a href='./create-account'>Create an Account</a></li>;
-        gameText = <li id="gametext"><a href="/selection">Play as a Guest</a></li>
+        userText =<li id="usertext">Welcome! <a href='./login'>Log In</a> or <a href='./create-account'>Create an Account</a></li>;
+        gameText = <li id="gametext"><a href="/selection">Play as a Guest</a></li>;
+        modifiedNav = <>
+                <li><a href='/'>Home</a></li>
+                <li><a href='./login'>Login</a></li>
+                <li><a href='./create-account'>Create Account</a></li>
+                <li><a href='/selection'>Play as a Guest</a></li></>
     } else { //can we bring the data forward in a better way?
         userText = <>
-        <li id="usertext">Welcome back, {user.firstName}! <SignOutButton /></li>
-        <li><a href="/results">See Previous Scores</a></li>
-        </>
-        gameText = <li id="gametext"><a href="/selection">Play</a> and beat your high score!</li>
+        <li className="usertext">Welcome back, {firstName}! </li>
+        <li className="usertext"><SignOutButton /></li>
+        <li className="usertext"><a href="/results">See Previous Scores</a></li>
+        </>;
+        gameText = <li id="gametext"><a href="/selection">Play</a> and beat your high score!</li>;
+        modifiedNav = <>
+        <li><a href='/'>Home</a></li>
+        <li><SignOutButton/></li>
+        <li><a href='./create-account'>Create Account</a></li>
+        <li><a href='/selection'>Play as a Guest</a></li>
+    </>;
     }
+
     return(
         <>
-            <ul>
+            <ul id="navbar">
                 {userText}
                 {gameText}
             </ul>
+            <div className={open ? "mobileNavbarOpen" : "mobileNavbarClosed"} >
+            <img id='menuIcon' src={menuicon} alt='Menu' onClick={()=>setOpen(!open)}></img>
+                <a href='/'>Home</a>
+                <a href='./login'>Login</a>
+                <a href='./create-account'>Create Account</a>
+                <a href='/selection'>Play as a Guest</a>
+            </div>
         </>
     );
 }
